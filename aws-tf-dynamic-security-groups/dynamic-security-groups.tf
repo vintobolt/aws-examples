056ef6da6aa88b67d5ab706b25da0fddd734953e
Oleksandr Zatserklianyi <vintobolt@protonmail.com>
2022-09-08 02:23:48 +0300

    add dynamic security groups

--- aws-tf-dynamic-security-groups/dynamic-security-groups.tf
+++ aws-tf-dynamic-security-groups/dynamic-security-groups.tf
@@ -21,3 +21,25 @@ resource "aws_instance" "my_webserver" {
     Owner = "vinto"
   }
 }
+
+resource "aws_security_group" "my_webserver" {
+  name = "my_webserver dynamic security group"
+  description = "simple dynamic security group"
+  vpc_id = aws_default_vpc.default.id
+  dynamic "ingress" {
+    for_each = ["80", "443"]
+    content {
+        from_port = ingress.value
+        to_port = ingress.value
+        protocol = "tcp"
+        cidr_blocks = [ "0.0.0.0/0" ]
+    }
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+}
