cfc70f25806f86f01eb4f2ef002e006d45470ab5
Oleksandr Zatserklianyi <vintobolt@protonmail.com>
2022-09-09 01:12:05 +0300

    add description in output

--- tf-output/output.tf
+++ tf-output/output.tf
@@ -1,7 +1,9 @@
 output "webserver-instance-id" {
-  value = aws_instance.my_webserver.id 
+  value = aws_instance.my_webserver.id
+  description = "print instance id"
 }
 
 output "webserver_elastic_public_ip" {
   value = aws_eip.my_static_ip.public_ip
+  description = "print public ip"
 }
