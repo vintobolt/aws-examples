468fac91636a9d378f3c10b4ff3c864a64679392
Oleksandr Zatserklianyi <vintobolt@protonmail.com>
2022-09-11 18:56:13 +0300

    Add vpc and security group

--- resource-creation-order/main.tf
+++ resource-creation-order/main.tf
@@ -4,3 +4,39 @@ provider "aws" {
   shared_credentials_files = ["$HOME/.aws/credentials"]
   profile                  = "lolcatbolt"
 }
+
+resource "aws_default_vpc" "default_vpc" {}
+
+resource "aws_security_group" "servers" {
+  name = "Security group"
+  description = "My security group"
+  vpc_id = aws_default_vpc.default_vpc.id
+
+  dynamic "ingress" {
+    for_each = ["80", "443"]
+    content {
+      from_port = ingress.value
+      to_port = ingress.value
+      protocol = "tcp"
+      cidr_blocks = [ "0.0.0.0/0" ]
+    }
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
+  tags = {
+    Name = "Servers security group"
+    Owner = "vintobot"
+  }
+}
+
+resource "aws_instance" "webserver" {
+    instance_type = "t2.micro"
+    ami           = "ami-090fa75af13c156b4"
+
+}
